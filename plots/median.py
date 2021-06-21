513935591295e41a0acc009450b1f716eb7549e4
Aleksei Dudchenko <aleksei.dudchenko@med.uni-heidelberg.de>
2021-06-21 22:16:59 +0200

    style refactoring

--- plots/median.py
+++ plots/median.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
 switcher = {
-        'Толчок ДЦ':'LC',
-        'Сумма дв-рья':'BI',
-        'Толчок':'Jerk',
-        'Рывок (сумма)':'Snatch_Summ'
+        'Толчок ДЦ': 'LC',
+        'Сумма дв-рья': 'BI',
+        'Толчок': 'Jerk',
+        'Рывок (сумма)': 'Snatch_Summ'
     }
+
 
 def draw_three_medians(df, discipline, years, save=False, dpi=80, test=False):
 
@@ -15,7 +16,7 @@ def draw_three_medians(df, discipline, years, save=False, dpi=80, test=False):
         medians_main.append(get_medians(df, year, discipline))
 
     #todo make possible to have different categories.
-    w_categories_str, w_categories_str = get_categoties(years[0])
+    w_categories_str, w_categories_str = get_categories(years[0])
 
     if discipline == "Сумма дв-рья":
         medians_snatch = []
@@ -25,14 +26,14 @@ def draw_three_medians(df, discipline, years, save=False, dpi=80, test=False):
             medians_snatch.append(get_medians(df, year, "Рывок (сумма)"))
             medians_jerk.append(get_medians(df, year, "Толчок"))
   
-    fig, axes = plt.subplots(1,3, figsize=(10,4))
+    fig, axes = plt.subplots(1, 3, figsize=(10, 4))
 
     for i in range(3):
         axes[i].plot(w_categories_str, medians_main[i], color='r', label='Сумма')
         axes[i].plot(w_categories_str, medians_main[i], 'ro')
         axes[i].set_title(years[i], color='g')
 
-    if discipline=='Сумма дв-рья':
+    if discipline == 'Сумма дв-рья':
         for i in range(3):
             axes[i].plot(w_categories_str, medians_snatch[i], color='g', label='Рывок')
             axes[i].plot(w_categories_str, medians_snatch[i], 'go')
@@ -55,12 +56,11 @@ def draw_three_medians(df, discipline, years, save=False, dpi=80, test=False):
         ax.set_ylim(get_ylim(discipline))
         ax.grid()
 
-
     fig.tight_layout(rect=[0, 0, 1, 0.95])
 
     if save:
         path = '../giristat/images/'
-        filename='Median_catagories_' + switcher.get(discipline, 'UN') + '_CR_'  + str(years)  + '.png'
+        filename = 'Median_catagories_' + switcher.get(discipline, 'UN') + '_CR_' + str(years) + '.png'
         plt.savefig(path+filename, dpi=dpi)
         
     plt.show(not test)
@@ -70,9 +70,9 @@ def draw_three_medians(df, discipline, years, save=False, dpi=80, test=False):
 def draw_median(df, discipline, year, save=False, dpi=80, test=False):
 
     medians = get_medians(df, year, discipline)
-    w_categories = get_categoties(year)
+    w_categories = get_categories(year)
    
-    fig, axes = plt.subplots(1,2, figsize=(10,5.5))
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5.5))
 
     for i in range(2):
         axes[i].plot(w_categories[i], medians, 'r')
@@ -93,34 +93,33 @@ def draw_median(df, discipline, year, save=False, dpi=80, test=False):
 
     if save:
         path = '../giristat/images/'
-        filename='Median_catagories_' + switcher.get(discipline, 'UN') + '_CR_'  + str(year)  + '.png'
+        filename = 'Median_catagories_' + switcher.get(discipline, 'UN') + '_CR_' + str(year) + '.png'
         plt.savefig(path+filename, dpi=dpi)
     
     plt.show(not test)
     return None
 
 
-
-def get_categoties(year):
+def get_categories(year):
     if year == 2017:
-        w_categories=[63, 68, 73, 78, 85, 95, 100]
-        w_categories_str=['63','68','73','78','85','95','95+']
+        w_categories = [63, 68, 73, 78, 85, 95, 100]
+        w_categories_str = ['63', '68', '73', '78', '85', '95', '95+']
     else:
-        w_categories=[63,68,73,85,100]
-        w_categories_str=['63','68','73','85','85+']
+        w_categories = [63, 68, 73, 85, 100]
+        w_categories_str = ['63', '68', '73', '85', '85+']
     return w_categories, w_categories_str
 
 
 def get_medians(df, year, discipline):
-    df_year = df[df['Год']==year]
+    df_year = df[df['Год'] == year]
 
-    df_year_63 = df_year[df_year['в/к']==63]
-    df_year_68 = df_year[df_year['в/к']==68]
-    df_year_73 = df_year[df_year['в/к']==73]
-    df_year_85 = df_year[df_year['в/к']==85]
-    df_year_999 = df_year[df_year['в/к']==999]
+    df_year_63 = df_year[df_year['в/к'] == 63]
+    df_year_68 = df_year[df_year['в/к'] == 68]
+    df_year_73 = df_year[df_year['в/к'] == 73]
+    df_year_85 = df_year[df_year['в/к'] == 85]
+    df_year_999 = df_year[df_year['в/к'] == 999]
 
-    medians=[]
+    medians = []
     medians.append(df_year_63[discipline].median())
     medians.append(df_year_68[discipline].median())
     medians.append(df_year_73[discipline].median())
@@ -128,22 +127,22 @@ def get_medians(df, year, discipline):
     medians.append(df_year_999[discipline].median())
 
     if year == 2017:
-        df_year_78 = df_year[df_year['в/к']==78]
-        df_year_95 = df_year[df_year['в/к']==95]
+        df_year_78 = df_year[df_year['в/к'] == 78]
+        df_year_95 = df_year[df_year['в/к'] == 95]
 
         medians.insert(3, df_year_78[discipline].median())
         medians.insert(5, df_year_95[discipline].median())
 
     return medians
-    
-def get_ylim(discipline):
-        if discipline=='Сумма дв-рья':
-            ylim=(0,260)
-        elif discipline=='Толчок ДЦ':
-            ylim=(20,90)
-        elif discipline=='Толчок':
-            ylim=(0,200)
-        else:
-            ylim=(0,300)
-        return ylim
 
+
+def get_ylim(discipline):
+    if discipline == 'Сумма дв-рья':
+        ylim = (0, 260)
+    elif discipline == 'Толчок ДЦ':
+        ylim = (20, 90)
+    elif discipline == 'Толчок':
+        ylim = (0, 200)
+    else:
+        ylim = (0, 300)
+    return ylim
