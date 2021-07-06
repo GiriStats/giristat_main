7080338024f4af6847fa1f5daf2f052c4eb864ac
Aleksei Dudchenko <aleksei.dudchenko@med.uni-heidelberg.de>
2021-07-06 13:27:11 +0200

    add new class for a data entry

--- data_processing/prepare_merged_data.py
+++ data_processing/prepare_merged_data.py
@@ -1,5 +1,35 @@
 import pandas as pd
 
+class DataEntry:
+    '''represents an entry in a protocol'''
+
+    def __init__(self):
+        self.name = None
+        self.birth = None
+        self.wc = None
+        self.weight = None
+        self.discipline = None
+        self.jerk = None
+        self.snatch = None
+        self.jerkLC = None
+        self.wc_t = None
+        self.team = None
+
+
+    def __init__(self, name, birth, wc, weight, discipline, jerk, snatch, jerkLC, wc_t, team):
+        self.name = name
+        self.birth = birth
+        self.wc = wc
+        self.weight = weight
+        self.discipline = discipline
+        self.jerk = jerk
+        self.snatch = snatch
+        self.jerkLC = jerkLC
+        self.wc_t = wc_t
+        self.team = team
+
+
+
 
 def get_mearged_df(file1, file2):
     # 'source/RC_LC_2017-20.csv'
@@ -43,6 +73,7 @@ def get_mearged_df(file1, file2):
 
     return df
 
+
 def get_df(file1):
     # All-RC-2017-20.csv
     df = pd.read_csv(file1)
@@ -75,4 +106,48 @@ def get_norms(year):
     norms = pd.read_csv('source/norms.csv')
     year_norms = norms[(norms['year_from'] == year) | (norms['year_to'] == year) | (norms['year2'] == year) | (norms['year3'] == year)]
     return year_norms
+
+
+
+def get_dataentries(df):
+    '''Load all entries from file to DataEntries'''
+    df = df.reset_index()
+    entry = []
+
+    for i in range(df.shape[0]):
+        entry.append(DataEntry(df.Name[i],
+                               df.BirthDate[i],
+                               df.WC[i],
+                               df.Weight[i],
+                               df.Discipline[i],
+                               df.Jerk[i],
+                               df.Snatch[i],
+                               df.JerkLC[i],
+                               df.WC_t[i],
+                               df.Team[i] ))
+
+
+    # for i in range(df.shape[0]):
+    # #     # print(i)
+    #     entry.append(DataEntry())
+    #     entry[i].name = df.Name[i]
+    #     entry[i].birth = df.BirthDate[i]
+    #     entry[i].team = df.Team[i]
+    #     entry[i].weight = df.Weight[i]
+    #     entry[i].jerk = df.Jerk[i]
+    #     entry[i].snatch = df.Snatch[i]
+    #     entry[i].jerkLC = df.JerkLC[i]
+    #     entry[i].discipline = df.Discipline[i]
+    #     entry[i].wc = df.WC[i]
+    #     entry[i].wc_t = df.WC_t[i]
+
+    # print(entry.__len__())
+
+    for i in range(entry.__len__()):
+        print(i, entry[i].name, entry[i].birth)
+
+    return entry
+
+
+
 
